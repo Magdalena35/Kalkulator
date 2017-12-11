fa75dda20591894d4ae67adefc1772019973335d
Magdalena35 <mada.szewczyk@gmail.com>
2017-12-11 22:36:54 +0100

    Kalkulator

---
+++ src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,14 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+    static void add(){}
+    static void substract(){}
+
+    public static void main (String [] args){
+        Calculator calculator = new Calculator();
+        calculator.add();
+        calculator.substract();
+
+    }
+
+}
