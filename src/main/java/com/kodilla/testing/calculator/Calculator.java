380f34d1fc60b888c3d61344bf4d13c3bbff82d0
Magdalena35 <mada.szewczyk@gmail.com>
2017-12-12 20:17:31 +0100

    Kalkulator

--- src/main/java/com/kodilla/testing/calculator/Calculator.java
+++ src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -1,13 +1,19 @@
 package com.kodilla.testing.calculator;
 
 public class Calculator {
-    static void add(){}
-    static void substract(){}
+
+    int add(int a, int b){
+        return (a+b);
+    }
+    int subtract(int a, int b){
+        return (a-b);
+
+    }
 
     public static void main (String [] args){
         Calculator calculator = new Calculator();
-        calculator.add();
-        calculator.substract();
+        calculator.add(4,2);
+        calculator.subtract(4,2);
 
     }
 
